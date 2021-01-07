2125812409d1bf0c05a69a0e65279516629b6f60
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-07 23:37:43 +0530

    updated routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,21 +1,29 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+//pages
 import Index from "../pages/Index";
 import SignUp from "../pages/SignUp";
 import Login from "../pages/Login";
+
+//components
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+// import LoginForm from "../components/LoginForm";
 
 class Router extends Component {
   render() {
     return (
       <div>
         <BrowserRouter>
+          <Header />
           <Switch>
             <Route path="/" exact component={Index} />
             <Route path="/signup" exact component={SignUp} />
             <Route path="/login" exact component={Login} />
           </Switch>
         </BrowserRouter>
+        <Footer />
       </div>
     );
   }
