fd3e025e478516094a387fe66a76919ea8acfd47
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-07 23:34:53 +0530

    created foooter element

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import footerStyles from "../styles/Footer.module.css";
+import { FaTwitter, FaFacebook, FaInstagram } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div id={footerStyles.copyright}>
+      <p>&copy; Designed and developed by Rutuja.</p>
+      <ul className={footerStyles.contact}>
+        <li>
+          <a href="/#">
+            <FaTwitter size="2em" color="cyan" />
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <FaFacebook size="2em" color="#00acee" />
+          </a>
+        </li>
+        <li>
+          <a href="/#">
+            <FaInstagram size="2em" className={footerStyles.insta} />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Footer;
