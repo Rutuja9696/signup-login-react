0dbf70781aaf6703facb1f2cd7831e158563ea99
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-07 23:38:51 +0530

    added style class to login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,33 @@
 import { Component } from "react";
 import LoginForm from "../components/LoginForm";
+import FormElementStyles from "../styles/Form.module.css";
 
 class Login extends Component {
   render() {
     return (
-      <div>
-        <form>
-          <div>
+      <div className={FormElementStyles.body}>
+        <div className={FormElementStyles.container}>
+          <form>
             <LoginForm />
-            <button>Login</button>
-            <button>Reset</button>
-          </div>
-        </form>
+            <div className={FormElementStyles.buttons}>
+              <button
+                type="submit"
+                value="login"
+                className={FormElementStyles.button}
+              >
+                Login
+              </button>
+
+              <button
+                type="reset"
+                value="Reset"
+                className={FormElementStyles.button}
+              >
+                Reset
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
