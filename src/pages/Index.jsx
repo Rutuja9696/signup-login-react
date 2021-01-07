e76a7dcad23777fa46095a68b1d5f2ddea3a5b4f
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-07 23:38:20 +0530

    added style class to index

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,15 +1,19 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
+import indexStyle from "../styles/Index.module.css";
 class Index extends Component {
   render() {
     return (
-      <div>
-        <Link to="/signup">
-          <button>Signup</button>
-        </Link>
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
+      <div className={indexStyle.divIndex}>
+        <h1 className={indexStyle.indexh1}>WELCOME !</h1>
+        <div className={indexStyle.divButton}>
+          <Link to="/signup">
+            <button>Signup</button>
+          </Link>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </div>
       </div>
     );
   }
