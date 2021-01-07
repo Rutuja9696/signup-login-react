298c75237814d8dd1a30e941302dfae7d50c7c1a
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-07 23:39:18 +0530

    added style class to signup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,17 +1,33 @@
 import { Component } from "react";
 import SignUpForm from "../components/SignUpForm";
+import FormElementStyles from "../styles/Form.module.css";
 
 class SignUp extends Component {
   render() {
     return (
-      <div>
-        <form>
-          <div>
+      <div className={FormElementStyles.body}>
+        <div className={FormElementStyles.container}>
+          <form>
             <SignUpForm />
-            <button>Submit</button>
-            <button>Reset</button>
-          </div>
-        </form>
+            <div className={FormElementStyles.buttons}>
+              <button
+                type="submit"
+                value="signUp"
+                className={FormElementStyles.button}
+              >
+                Sign Up
+              </button>
+
+              <button
+                type="reset"
+                value="Reset"
+                className={FormElementStyles.button}
+              >
+                Reset
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
