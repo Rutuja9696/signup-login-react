f5ea0f772fa2eac40fe6b215504fb32b469f5bd6
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-09 12:25:09 +0530

    added onclick function for signup button

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,11 +3,38 @@ import SignUpForm from "../components/SignUpForm";
 import FormElementStyles from "../styles/Form.module.css";
 
 class SignUp extends Component {
+  signupUser = (event) => {
+    event.preventDefault();
+    let form = event.target;
+    let formObj = new FormData();
+    formObj.append("name", form.name.value);
+    formObj.append("email", form.email.value);
+    formObj.append("password", form.password.value);
+
+    fetch(url, {
+      method: "POST",
+      mode: "cors",
+      body: formObj,
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        form.reset();
+        alert("Signed up Successfully");
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(`${form.name.value}, Please try again :)`);
+        alert(err);
+        console.log(err);
+      });
+  };
   render() {
     return (
       <div className={FormElementStyles.body}>
         <div className={FormElementStyles.container}>
-          <form>
+          <form onSubmit={this.signupUser}>
             <SignUpForm />
             <div className={FormElementStyles.buttons}>
               <button
