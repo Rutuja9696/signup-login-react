52cba2e37270c282da7998e7af34156e733be1aa
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-09 12:39:32 +0530

    added on click loginuser function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,11 +3,37 @@ import LoginForm from "../components/LoginForm";
 import FormElementStyles from "../styles/Form.module.css";
 
 class Login extends Component {
+  loginUser = (event) => {
+    event.preventDefault();
+    let form = event.target;
+    let formObj = new FormData();
+    formObj.append("email", form.email.value);
+    formObj.append("password", form.password.value);
+
+    fetch(url, {
+      method: "POST",
+      mode: "cors",
+      body: formObj,
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        form.reset();
+        alert("Logged in Successfully");
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(`${form.name.value}, Please try to sign up first:)`);
+        alert(err);
+        console.log(err);
+      });
+  };
   render() {
     return (
       <div className={FormElementStyles.body}>
         <div className={FormElementStyles.container}>
-          <form>
+          <form onSubmit={this.loginUser}>
             <LoginForm />
             <div className={FormElementStyles.buttons}>
               <button
